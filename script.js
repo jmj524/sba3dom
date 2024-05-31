e2c2ab5c08ebb15feccae1535ec95a5b58cc4e69
James Johnson <jmjohnson524@yahoo.com>
2024-05-31 11:28:05 -0400

    sba3

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+  
+  function myFunction() {
+    var x = document.getElementById("mySelect").length;
+    document.getElementById("demo").innerHTML = x;
+  }
+  const myanswer = "Florida";
+  
+ {
+    let Ohio = Georgia = Alabama;
+  console.log(Florida);
+ }
+ console.log(Ohio)
+  
